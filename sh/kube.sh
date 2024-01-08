76be71d2f430caed620596914a1d3346d2ed790a
prince_x <ajjuammy88@gmail.com>
2024-01-08 19:04:33 +0000

    sh

---
+++ sh/kube.sh
@@ -0,0 +1,3 @@
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.49.0/deploy/static/provider/baremetal/deploy.yaml
